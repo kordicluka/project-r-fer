57f1fee555a86343abefa2cdbba396835a126b26
markomaslac <mm55649@fer.hr>
2025-11-04 01:23:34 +0100

    Update SimulationEnvironment.m

--- SimulationEnvironment.m
+++ SimulationEnvironment.m
@@ -1,4 +1,4 @@
-classdef SimulationEnvironment
+classdef SimulationEnvironment < handle
     properties
         % Simulacijski parametri
         Ts              % vremenski korak
@@ -31,6 +31,82 @@ classdef SimulationEnvironment
 
             obj.results = struct('T', [], 'X', [], 'U', []);
         end
-    end
 
+        function run(obj)
+            % Inicijalizacija
+            t = 0;
+            x = obj.x0;
+            Tstep = obj.Ts;
+        
+            % Spremanje početnih vrijednosti
+            obj.results.T = t;
+            obj.results.X = x';
+            obj.results.U = [];
+        
+            % Glavna petlja
+            while t < obj.T_end
+                % Izračun ulaza pomoću kontrolera
+
+                % u = obj.controller.izracunaj_silu(x);  
+        
+                % Dodavanje poremećaja
+                d = obj.disturbance;
+                % u = u + d;
+                u = 0; % test
+        
+                % Simulacija sustava preko modela
+                [x_next, ~] = obj.model.simulateStep(x, u, Tstep);
+        
+                % Ažuriranje vremena i stanja
+                t = t + Tstep;
+                x = x_next;
+        
+                % Spremi rezultate
+                obj.results.T = [obj.results.T; t];
+                obj.results.X = [obj.results.X; x'];
+                obj.results.U = [obj.results.U; u];
+            end
+        end
+
+        function animate(obj)
+            X = obj.results.X;
+            T = obj.results.T;
+        
+            % Položaj kolica (x) i kut njihala (theta)
+            px = X(:, 1);       % Položaj kolica (x-os)
+            theta = X(:, 2);    % Kut njihala
+        
+            % Parametri
+            l = obj.model.params.l; % Duljina njihala
+            cart_width = 0.3;       % Širina kolica
+            cart_height = 0.15;     % Visina kolica
+        
+            % Postavljanje koordinatnog sustava
+            figure;
+            hold on;
+            axis equal;
+            grid on;
+            xlim([-2 2]);
+            ylim([-1.2 1.2]);
+            xlabel('Položaj kolica [m]');
+            ylabel('Visina [m]');
+            title('Simulacija njihala na kolicima');
+        
+            % Inicijalni crtež
+            cart = rectangle('Position',[px(1)-cart_width/2, -cart_height/2, cart_width, cart_height], ...
+                             'FaceColor',[0.1 0.4 0.8]);
+            pendulum = line([px(1), px(1)+l*sin(theta(1))], ...
+                            [0, -l*cos(theta(1))], 'Color','r','LineWidth',2);
+        
+            % Animacija
+            for k = 1:10:length(T)
+                % Ažuriraj položaj
+                set(cart, 'Position', [px(k)-cart_width/2, -cart_height/2, cart_width, cart_height]);
+                set(pendulum, 'XData', [px(k), px(k)+l*sin(theta(k))]);
+                set(pendulum, 'YData', [0, -l*cos(theta(k))]);
+                drawnow;
+            end
+        end
+
+    end
 end
